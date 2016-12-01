03cb10fdddc5cf404f8f0264d9210d11da5ab61d
nature613 <chikonakamura613@gmail.com>
2016-12-01 14:28:35 +0100

    Change default view to use styled components instead of stylesheets.

--- index.ios.js
+++ index.ios.js
@@ -7,47 +7,42 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View
 } from 'react-native';
+import styled from 'styled-components/native';
 
-export default class bbqApp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #F5FCFF;
+`;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const Welcome = styled.Text`
+  font-size: 20;
+  text-align: center;
+  margin: 10;
+`;
 
+const Instructions = styled.Text`
+  text-align: center;
+  color: #333333;
+  margin-bottom: 5;
+`;
+
+const bbqApp = () => (
+  <Container>
+    <Welcome>
+      Welcome to React Native!
+    </Welcome>
+    <Instructions>
+      To get started, edit index.ios.js
+    </Instructions>
+    <Instructions>
+      Press Cmd+R to reload,{'\n'}
+      Cmd+D or shake for dev menu
+    </Instructions>
+  </Container>
+);
+
+export default bbqApp;
 AppRegistry.registerComponent('bbqApp', () => bbqApp);
