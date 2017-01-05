b814c46c2158c337a6975441f9f8745c05ccdbe9
nature613 <chikonakamura613@gmail.com>
2017-01-05 17:16:06 +0100

    Update versions
    Update versions in yarn.lock and package.json
    
    Run react-native upgrade

--- android/app/src/main/java/com/bbqapp/MainApplication.java
+++ android/app/src/main/java/com/bbqapp/MainApplication.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import com.facebook.react.ReactApplication;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.cmcewen.blurview.BlurViewPackage;
-import com.cmcewen.blurview.BlurViewPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -29,7 +28,6 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new LinearGradientPackage(),
-            new BlurViewPackage(),
             new BlurViewPackage()
       );
     }
