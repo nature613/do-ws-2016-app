a8eadbe3062dfb990b99feaee8e4d0ed73c6dbe2
nature613 <chikonakamura613@gmail.com>
2017-01-07 17:00:37 +0100

    Style timer.

--- app/components/Recipe.js
+++ app/components/Recipe.js
@@ -15,7 +15,7 @@ import {
 } from './index';
 
 const Button = styled.TouchableHighlight`
-  flex: 1;
+  flex: ${({flex}) => flex};
   border-width: 1;
   border-radius: 5;
   border-color: ${({disabled, content}) => {
@@ -27,16 +27,17 @@ const Button = styled.TouchableHighlight`
       }
       return 'rgb(0, 122, 255)'
     }};
-  margin: 20;
+  margin: 10;
 `;
 
-const IconButton = ({onPress, disabled, icon, content}) => {
+const IconButton = ({onPress, disabled, icon, content, flex = 1}) => {
   return (
     <Button
       underlayColor="#D0D0D0"
       onPress={onPress}
       disabled={disabled}
       content={content}
+      flex={flex}
     >
         <View><ButtonText disabled={disabled} content={content}>{content ? content : <Icon name={icon} size={30}/> }</ButtonText></View>
     </Button>
@@ -72,6 +73,8 @@ const ButtonText = styled(Center)`
     return 'rgb(0, 122, 255)'
   }};
   font-size: 25;
+  font-family: Courier New;
+  margin:5;
 `;
 const Label = styled(Text)`
   min-width: 150;
@@ -121,6 +124,19 @@ const Intensity = ({data}) => {
       return null;
   }
 }
+const Duration = ({data}) => {
+  let duration = data;
+  let unit = 'sec';
+  if (duration > 60) {
+    unit = 'min';
+    duration /= 60;
+    if (duration > 60) {
+      unit = 'h';
+      duration /= 60;
+    }
+  }
+  return <LightText>{new Date(data * 1000).toISOString().substr(11, 8)}</LightText>;
+}
 
 const Task = ({task, id, active}) => (
   <Step active={active}>
@@ -139,7 +155,7 @@ const Task = ({task, id, active}) => (
     </Conditional>
     <Conditional condition={task.duration}>
       <Label>Duration</Label>
-      <LightText>{task.duration} sec</LightText>
+      <Duration data={task.duration}/>
     </Conditional>
   </Step>
 )
@@ -154,6 +170,7 @@ class Sequence extends React.Component {
     super(props);
     this.updateTask = this.updateTask.bind(this)
     this.startTimer = this.startTimer.bind(this)
+    this.clearTimer = this.clearTimer.bind(this)
   }
   componentWillMount() {
     this.sound = new Sound('alarm.mp3', Sound.MAIN_BUNDLE, (e) => {
@@ -169,6 +186,7 @@ class Sequence extends React.Component {
       const { updateRoute, task, sequence} = this.props;
       const newTask = task + value;
       if((newTask>= 0) && (newTask < sequence.length)) {
+        this.clearTimer()
         updateRoute({
           task: newTask,
           ticks: null,
@@ -180,14 +198,18 @@ class Sequence extends React.Component {
   startTimer(duration){
     return () => {
       if (!this.timer) {
-        const durationMS = duration * 1000;
+        let interval = 1000;
         let ticks = duration;
+        const durationMS = duration * interval;
+        this.props.updateRoute({
+          ticks,
+        })()
         this.interval = setInterval(() => {
           --ticks;
           this.props.updateRoute({
             ticks,
           })()
-        }, 1000);
+        }, interval);
         this.timer = setTimeout(() => {
           clearInterval(this.interval);
           this.interval = null;
@@ -213,7 +235,21 @@ class Sequence extends React.Component {
           )
           this.timer = null;
         }, durationMS);
+      }else {
+        this.props.updateRoute({
+          ticks: null,
+        })()
+        this.clearTimer();
       }
+    }
+  }
+
+  clearTimer(){
+    if (this.timer) {
+      clearTimeout(this.timer);
+      this.timer = null;
+      clearInterval(this.interval);
+      this.interval = null;
     }
   }
 
@@ -224,10 +260,7 @@ class Sequence extends React.Component {
   }
 
   componentWillUnmount() {
-    if (this.timer) {
-      clearTimeout(this.timer);
-      clearInterval(this.interval);
-    }
+    this.clearTimer();
     if (this.sound) {
       this.sound.release();
     }
@@ -262,7 +295,8 @@ class Sequence extends React.Component {
             onPress={this.startTimer(sequence[activeTask].duration)}
             icon="ios-timer-outline"
             disabled={!sequence[activeTask].duration}
-            content={ticks}
+            content={ticks?(new Date(ticks * 1000).toISOString().substr(11, 8)):null}
+            flex={2}
           />
           <IconButton
             onPress={this.updateTask(1)}
